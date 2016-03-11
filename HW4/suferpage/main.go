648538efd219e54877a0f2133f70afddcaf8df4a
deepthi261 <deepthithota@outlook.com>
2016-03-11 01:24:17 -0800

    Updates

--- HW4/suferpage/main.go
+++ HW4/suferpage/main.go
@@ -8,7 +8,8 @@ import (
 
 // surferPage renders surfer.html page by putting the page content into the response back to client.
 func surferPage(res http.ResponseWriter, req *http.Request) {
-	temp, err := template.ParseFiles("./surfer.html")
+	temp := template.New("surfer.html")
+	temp, err := template.ParseFiles("surfer.html")
 	// Logging possible errors
 	logError(err)
 	temp.Execute(res, nil)
@@ -19,7 +20,7 @@ func main() {
 	// Registering the URL path and binding it to surferPage handler
 	http.HandleFunc("/", surferPage)
 
-	http.Handle("/css/", http.StripPrefix("/css", http.FileServer(http.Dir("./css"))))
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
 
 	// Setting the listener on port 8080
 	log.Println("Listening to 8080 ...")
