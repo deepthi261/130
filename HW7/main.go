13bfb9ddb0704f32a1821f0c97f969ff7c608110
deepthi261 <deepthithota@outlook.com>
2016-03-14 10:31:21 -0700

    Updates

---
+++ HW7/main.go
@@ -0,0 +1,18 @@
+
+package main
+
+import (
+	"net/http"
+	"io"
+)
+
+func main(){
+	http.Handle("/favicon.ico",http.NotFoundHandler())
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request){
+		key := "q"
+		val := req.FormValue(key)
+		io.WriteString(res, "Query String - Name :"+val)
+	})
+
+	http.ListenAndServe(":8080",nil)
+}
